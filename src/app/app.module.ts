34183ce9cbf37d8261669814dad4136a7bb4b1c7
Mahmoud Atia <Matia@cloudinn.net>
2021-09-17 20:26:55 +0200

    Add training phases components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MeterialModule } from './material.module';
 import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component'
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentTrainingsComponent } from './training/current-trainings/current-trainings.component';
+import { NewTrainingsComponent } from './training/new-trainings/new-trainings.component';
+import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    TrainingComponent,
+    CurrentTrainingsComponent,
+    NewTrainingsComponent,
+    PastTrainingsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/training/new-trainings/new-trainings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-trainings',
+  templateUrl: './new-trainings.component.html',
+  styleUrls: ['./new-trainings.component.scss']
+})
+export class NewTrainingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
